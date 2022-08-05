1bd53b06eed32ee2dab241e804912147f16e6313
Sanket <sanket@Sankets-Mac-mini.local>
2022-08-05 18:52:13 +0530

    WIP: api creaction for trello

--- app.js
+++ app.js
@@ -1,85 +1,85 @@
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
-
-import express, { response } from 'express'
-import bodyParser from 'body-parser'
-
+import express, { response } from "express";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT;
-const app = express()
+const app = express();
 
 // Indicate the Express.js that yo're using an additional plugin to trear parameters.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
-
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   response.send("Welcome on the books API! Take a breath and start using it!");
-})
-
+});
 
 // Static variable containing your books
-let bookList = [
-  'Make Time: How to Focus on what Matters Every Day',
-  'The Power Of Habit',
-]
+let Lists = [
+  {
+    id: 1,
+    title: "List 1",
+    isAddCard: false,
+    cardList: [
+      {
+        id: 1,
+        cardTitle: "Card 1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "List 2",
+    isAddCard: false,
+    cardList: [],
+  },
+];
 
+app.get("/dashboard_lists", (requset, response) => {
+  response.send({ lists: Lists });
+});
 
-app.get('/books', (requset, response) => {
-  return response.json({ allBooks: bookList})
-})
-
-
-// post the book
-app.post('/books', (request, response)=>{
-  const bookName = request.body.name
+// create a list
+app.post("/create_list", (request, response) => {
+  const bookName = request.body.name;
   //Check if it contain the book?
-  if(bookList.includes(bookName)) return response.json({sucess: false})
+  if (Lists.includes(bookName)) return response.json({ sucess: false });
 
-  bookList.push(bookName)
-  return response.json({success: true})
-})
+  Lists.push(bookName);
+  return response.json({ success: true });
+});
 
+//add a card
+app.post("/create_card", (request, response) => {
+  const bookName = request.body.name;
 
+  return response.json({ success: true });
+});
 
-// Delete the book
-app.delete('/books', (request, response)=> {
-  const bookToDelete = request.body.name
+// delete a list
+app.delete("/delete_list", (request, response) => {
+  const bookToDelete = request.body.name;
 
-  bookList = bookList.filter((book) => book !== bookToDelete)
+  Lists = Lists.filter((book) => book !== bookToDelete);
 
-  return response.json({allBooks: bookList})
-})
+  return response.json({ allBooks: Lists });
+});
 
+// delete a card
+app.delete("/delete_card", (request, response) => {
+  
+  return response.json({ allBooks: Lists });
+});
 
-// Update the Book 
-app.put('/books', (request, response) => {
-  const bookToUpdate = request.body.bookToUpdate
-  const bookUpdated = request.body.bookUpdated
-
-  let indexOfBook=-1;
-
-  let counter = 0;
-  for(let i in bookList){
-    if(bookList[i]==bookToUpdate){
-      indexOfBook = counter;
-      break;
-    }
-    counter++;
-  }
-
-
-  if(indexOfBook === -1){
-    return response.json({ "success": false })
-  }
-
-  bookList[indexOfBook] = bookUpdated
-
-  return response.json({ "allBooks" : bookList})
-})
-
+// Update a card
+app.put("/update_card_data", (request, response) => {
+  
+  return response.json({ allBooks: Lists });
+});
 
 // Listen the server
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`The Books API is running on: http://localhost:${PORT}.`);
-})
+});
